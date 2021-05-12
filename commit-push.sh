2d5cd1c6fdd0254d12b7f41faf5eb7b7ce283c7a
Jeremy Carter <jeremy@jeremycarter.ca>
2021-05-12 00:37:13 -0400

    Fix CI/CD.

--- commit-push.sh
+++ commit-push.sh
@@ -1,6 +1,60 @@
 #!/bin/bash
+# Commit and push all project git repos.
 
-git add .
-git commit -m "Update submodules."
-git push
+hobnob_commit_push() {
+  msg="$@"
 
+  cd libhob
+  git add .
+  git commit -m "$msg"
+  git push
+  cd ..
+
+  cd hob
+  git add .
+  git commit -m "$msg"
+  git push
+  cd ..
+
+  cd libhov
+  git add .
+  git commit -m "$msg"
+  git push
+  cd ..
+
+  cd hov
+  git add .
+  git commit -m "$msg"
+  git push
+  cd ..
+
+  cd libnob
+  git add .
+  git commit -m "$msg"
+  git push
+  cd ..
+
+  cd nob
+  git add .
+  git commit -m "$msg"
+  git push
+  cd ..
+
+  cd libnov
+  git add .
+  git commit -m "$msg"
+  git push
+  cd ..
+
+  cd nov
+  git add .
+  git commit -m "$msg"
+  git push
+  cd ..
+
+  git add .
+  git commit -m "$msg"
+  git push
+}
+
+hobnob_commit_push "$@"
