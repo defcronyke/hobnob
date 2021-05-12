6a4f2396b9ff6916d7da1a9601b759ec223c8277
Jeremy Carter <jeremy@jeremycarter.ca>
2021-05-12 00:40:16 -0400

    Fix CI/CD.

--- commit-push.sh
+++ commit-push.sh
@@ -6,54 +6,54 @@ hobnob_commit_push() {
 
   cd libhob
   git add .
-  git commit -m "$msg"
+  git commit -m "$msg" && \
   git push
   cd ..
 
   cd hob
   git add .
-  git commit -m "$msg"
+  git commit -m "$msg" && \
   git push
   cd ..
 
   cd libhov
   git add .
-  git commit -m "$msg"
+  git commit -m "$msg" && \
   git push
   cd ..
 
   cd hov
   git add .
-  git commit -m "$msg"
+  git commit -m "$msg" && \
   git push
   cd ..
 
   cd libnob
   git add .
-  git commit -m "$msg"
+  git commit -m "$msg" && \
   git push
   cd ..
 
   cd nob
   git add .
-  git commit -m "$msg"
+  git commit -m "$msg" && \
   git push
   cd ..
 
   cd libnov
   git add .
-  git commit -m "$msg"
+  git commit -m "$msg" && \
   git push
   cd ..
 
   cd nov
   git add .
-  git commit -m "$msg"
+  git commit -m "$msg" && \
   git push
   cd ..
 
   git add .
-  git commit -m "$msg"
+  git commit -m "$msg" && \
   git push
 }
 
